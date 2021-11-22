e5c606c9d95ea2d039917b9ede37f21cc4e7e69b
Nestorer <202105913@post.au.dk>
2021-11-22 16:16:36 +0100

    REFACTOR FIB.C

--- src/fib.c
+++ src/fib.c
@@ -11,16 +11,12 @@
 // where fib(1) = 1, fib(2)=1, fib(3) = 2, etc.
 
 // where old fib(1) is now fib(1, 1, 0) and old fib(2) is now fib(2, 1, 0), etc.
+
 int fib(int n, int p, int pp)
 {
-    assert(n >= 1);
+    assert(n >= 1); // if n = 0, we don't want any numbers. Not even a return of 0. So we just ignore this.
 
     if (n == 1)
         return p;
-
-    int t = pp;
-    pp = p;
-    p = p + t;
-
-    return fib(n - 1, p, pp);
+    return fib(n - 1, p + pp, p);
 }
